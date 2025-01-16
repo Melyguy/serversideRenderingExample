d1db293da7ae14364e39eb48b013b8c37b8bad03
Odin-Johan <nidojohan@gmail.com>
2025-01-16 09:28:24 +0100

    W commit

---
+++ index.js
@@ -0,0 +1,16 @@
+    const express  = require('express');
+
+    const app = express();
+
+    app.get('/',(req,res) => {
+        res.send('Hello world! Klokken er ' + new Date().toLocaleTimeString());
+    })
+    app.get('/her',(req,res) => {
+        res.send(`
+            <h1>Her er en overskrift</h1>
+            <p>og her er et paragraf</p>
+        `);
+    })
+    app.listen(3000, () =>{
+        console.log('Server is listening on port 3000')
+    })
