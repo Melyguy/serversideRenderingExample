8ab227462110a5c7e311ff95239ab2d98d92d02c
Odin-Johan <nidojohan@gmail.com>
2025-01-21 10:42:08 +0100

    safe commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
     const express  = require('express');
 
     const app = express();
+    const { Pool } = require('pg');
+
+    const pool = new Pool({
+        user: 'postgres',
+        password: 'mysecretpassword',
+        host: 'localhost',
+        port: 5432,
+    });
 
     app.get('/',(req,res) => {
         res.send('Hello world! Klokken er ' + new Date().toLocaleTimeString());
@@ -20,6 +28,19 @@
                 <li>Skibidi</li>
             </ul>
         `);
+    })
+    app.get('/deltakereLive', async (req,res) => {
+            const result = await pool.query('SELECT * FROM users');
+
+            let html = "<h1>Deltagere</h1>"
+            html += "<ul>"
+
+            for( const row of result.rows){
+                html += "<li>" + row.name + "</li>"
+            }
+
+            html += "</ul>"
+            res.send(hmtl);
     })
     app.get('/api',(req, res) => {
         res.json({message: 'Hello World!'})
