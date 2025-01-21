be8dc68849fe3eb01f6152c99a0022835e9b5445
Odin-Johan <nidojohan@gmail.com>
2025-01-21 10:22:53 +0100

    begynner på oopgave 2

--- index.js
+++ index.js
@@ -21,6 +21,9 @@
             </ul>
         `);
     })
+    app.get('/api',(req, res) => {
+        res.json({message: 'Hello World!'})
+    });
     app.listen(3000, () =>{
         console.log('Server is listening on port 3000')
     })  
