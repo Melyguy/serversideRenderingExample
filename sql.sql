2c2f4683469713359a21e23542a166ae69fcc915
Odin-Johan <nidojohan@gmail.com>
2025-01-29 10:03:40 +0100

    postgres fungerer fortsatt ikke

--- sql.sql
+++ sql.sql
@@ -2,13 +2,15 @@ CREATE TABLE IF NOT EXISTS users  (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
 );
+CREATE TABLE filmer (
+    id SERIAL PRIMARY KEY,
+    tittel VARCHAR(100) NOT NULL
+);
 
-INSERT INTO users (name) VALUES
-    ('Joe Biden'),
-    ('Donald Trump'),
-    ('Herman Lindalen Nodeland'),
-    ('superman'),
-    ('Kamala Harris');
+CREATE TABLE skuespillere (
+    id SERIAL PRIMARY KEY,
+    navn VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE Cars (
     id SERIAL PRIMARY KEY,
@@ -19,3 +21,22 @@ INSERT INTO Cars (merke) VALUES
     ('BMW'),
     ('Toyota'),
     ('Volvo');
+
+INSERT INTO users (name) VALUES
+    ('Joe Biden'),
+    ('Donald Trump'),
+    ('Herman Lindalen Nodeland'),
+    ('superman'),
+    ('Kamala Harris');
+    
+
+INSERT INTO filmer (tittel) VALUES
+    ('The Matrix'),
+    ('The Matrix Reloaded'),
+    ('The Matrix Revolutions');
+
+INSERT INTO skuespillere (navn) VALUES
+    ('Keanu Reeves'),
+    ('Laurence Fishburne'),
+    ('Carrie-Anne Moss');
+                        
